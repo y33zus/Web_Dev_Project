c7566b88568b59b315a17e5dce3870ef91251f80
lil_landau <80888469+LilLandau@users.noreply.github.com>
2024-04-22 23:22:25 +0500

    Django project is initiated

--- Project/Web_Dev_Project/src/app/app.component.ts
+++ Project/Web_Dev_Project/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Web_Dev_Project';
+  niggers: number[];
+
+  constructor() {
+    this.niggers = [1, 2, 3];
+  }
 }
