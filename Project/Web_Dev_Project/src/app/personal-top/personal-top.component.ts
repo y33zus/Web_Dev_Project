97e90fe5b08fda2d48f19b598b6cea424980e814
y33zus <z_zholdasbay@kbtu.kz>
2024-04-23 00:26:52 +0500

    Creating components

---
+++ Project/Web_Dev_Project/src/app/personal-top/personal-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-top.component.html',
+  styleUrl: './personal-top.component.css'
+})
+export class PersonalTopComponent {
+
+}
---
+++ Project/Web_Dev_Project/src/app/watched-list/watched-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watched-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './watched-list.component.html',
+  styleUrl: './watched-list.component.css'
+})
+export class WatchedListComponent {
+
+}
